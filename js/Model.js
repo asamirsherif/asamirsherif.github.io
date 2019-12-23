509c3f36219af4ba887d70ebbbfd1988db30b3b4
asamirsherif <54451098+asamirsherif@users.noreply.github.com>
2019-12-24 00:12:34 +0200

    initial commit

---
+++ js/Model.js
@@ -0,0 +1,24 @@
+/**
+ * The goal is to match pairs of cards in the least
+ * number of matching attempts.
+ * Model
+ */
+
+/**
+ * @namespace Card object
+ */
+MemoryGame.Card = function(value, isMatchingCard) {
+  this.value = value;
+  this.isRevealed = false;
+  if (isMatchingCard) {
+    this.isMatchingCard = true;
+  }
+
+  this.reveal = function() {
+    this.isRevealed = true;
+  }
+
+  this.conceal = function() {
+    this.isRevealed = false;
+  }
+};
